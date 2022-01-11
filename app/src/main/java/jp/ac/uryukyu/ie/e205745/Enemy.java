f5dacf61564caaccc699fbec3ee7780fb9768319
Hiroshi KATAOKA <e205745@ie.u-ryukyu.ac.jp>
2022-01-11 13:57:39 +0900

    Step4

--- app/src/main/java/jp/ac/uryukyu/ie/e205745/Enemy.java
+++ app/src/main/java/jp/ac/uryukyu/ie/e205745/Enemy.java
@@ -5,7 +5,7 @@ package jp.ac.uryukyu.ie.e205745;
  *  String name; //敵の名前
  *  int hitPoint; //敵のHP
  *  int attack; //敵の攻撃力
- *  boolean dead; //敵の生死状態。true=死亡。*  getName(); //敵の名前を返す。
+ *  boolean dead; //敵の生死状態。true=死亡。
  * Created by tnal on 2016/11/13.
  */
 public class Enemy {
